9094862ed55b8d4ab58f7a1c16c46045e496ce69
georgex1 <grandgeorgex@hotmail.com>
2016-02-01 13:49:36 -0300

    link

--- js/index.js
+++ js/index.js
@@ -3612,7 +3612,7 @@ function open_fb_share(){
     facebookConnectPlugin.showDialog( 
     {
         method: "feed",
-        redirect_uri: 'http://thepastoapps.com/proyectos/myrugby/xhtml_verimg/index.php?partidos_id=' + partido.id,
+        link: 'http://thepastoapps.com/proyectos/myrugby/xhtml_verimg/index.php?partidos_id=' + partido.id,
         picture:'http://thepastoapps.com/proyectos/myrugby/webservices/shares/share'+ partido.id +'.png',
         name:'My Rugby!',
         message:'Resultados del Partido',    
